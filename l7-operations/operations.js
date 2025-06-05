6f7a217810a3e8e200467381c77c088912c31810
Ashwani singh <ashwani25122004@gmail.com>
2025-06-05 22:40:44 +0530

    about basic operations and conversions

---
+++ l7-operations/operations.js
@@ -0,0 +1,27 @@
+// ------------operations---------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// basic maths operations
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello "
+let str2 = "Ashwani"
+console.log(str1+str2) // string concatination
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2") // confusing conversion
+// dekho bhai simply javascript me aisa conversion bhi hota hai ki agar string pahale aa rahi hai to sb string me convert ho jaayega or agar last me aa raha hai to pahale ke operations execute honge phir uske baad string me convert hoga.
+
+console.log(true) // but when we write
+console.log(+true) // + ye likhne se number me conversion ho jaata hai but hum itna complex code likhte nhi hai .
+console.log(+"")
