a21b508aaefcaf13fe124b99323e4c016066a45b
Ashwani singh <ashwani25122004@gmail.com>
2025-07-24 22:25:40 +0530

    While & Do-While Loop

---
+++ l28-While&DoWhile/While&DoWhile.js
@@ -0,0 +1,22 @@
+// // while-loop
+
+// // initialization
+// // while(condition){
+// //     code expression
+// //     updation
+// // }
+
+// let i = 0
+// while(i<=10){
+//     console.log(`value of i is ${i}`)
+//     i++
+// }
+
+
+//                                                  Do-While loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+}while (score <= 10)
