e335cc9a984a7b2d46afdcc57dea42f1fc9f0268
Ashwani singh <ashwani25122004@gmail.com>
2025-07-24 22:14:07 +0530

    For Loop

---
+++ l27-ForLoop/forLoop.js
@@ -0,0 +1,27 @@
+// loops is also know as iterations
+
+// for-loop
+
+// for(let i = 0; i<=10; i++){ // note: here const can not be used
+//     console.log(i)
+// }
+
+// for ( let i = 0; i <= 1; i++){
+//     console.log(`outer loop value ${i}`)
+//     for (let j = 0; j <= 1; j++){
+//         console.log(`Inner loop value ${j}`)
+//     }
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// for(let i = 0; i < myArray.length; i++){
+//     console.log(myArray[i])
+// }
+
+// //                                          Break and Continue
+
+// for(let i = 0; i <= 10; i++){
+//     if(i == 3) continue
+//     if(i == 5) break
+//     console.log(i)
+// }
