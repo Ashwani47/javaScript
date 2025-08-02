0973c5cb36c533c1682e9c250d07e01adf25d647
Ashwani singh <ashwani25122004@gmail.com>
2025-08-02 12:25:55 +0530

    Comparison operators

--- l08-comparisons/comparisons.js
+++ l08-comparisons/comparisons.js
@@ -12,7 +12,7 @@ console.log("2" < "1") // isko bhi but aise na hi kare to better hai
 console.log(null > 0)
 console.log(null < 0)
 console.log(null == 0)
-console.log(null >= 0) // why true?
+console.log(null >= 0) // why true? 
 // the reason is that equality check == and comparison checks > < >= <= works differently.
 // comparisons convert null to anumber that is 0. that's why null >= 0 is true and null > 0 is false.
 
